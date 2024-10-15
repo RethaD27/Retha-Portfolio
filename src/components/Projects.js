c572e9341903bede90702ad85d7c9a6dd202e0de
Rethabile <rethadiale27@gmail.com>
2024-10-15 16:10:48 +0200

    "Updated SVG icons, modified components (Banner, Footer, NavBar, Projects, Skills) and removed MailchimpForm and Newsletter components."

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -60,9 +60,6 @@ export const Projects = () => {
                     <Nav.Item>
                       <Nav.Link eventKey="second">Tab 2</Nav.Link>
                     </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                     <Tab.Pane eventKey="first">
@@ -73,16 +70,13 @@ export const Projects = () => {
                               <ProjectCard
                                 key={index}
                                 {...project}
-                                />
+                              />
                             )
                           })
                         }
                       </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
+                    <Tab.Pane eventKey="second">
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
                   </Tab.Content>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,7 +10,6 @@ import colorSharp from "../assets/img/color-sharp.png"
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5
     },
@@ -34,31 +33,51 @@ export const Skills = () => {
             <div className="row">
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                        <h2>Tech Stack</h2>
+                        <p>Here are some of the technologies and tools I work with:</p>
+                        <Carousel 
+                          responsive={responsive} 
+                          infinite={true} 
+                          className="owl-carousel owl-theme skill-slider"
+                        >
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <img src={meter1} alt="Web Development" />
+                                <h5>React.js</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <img src={meter2} alt="Brand Identity" />
+                                <h5>Vue.js</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <img src={meter3} alt="Logo Design" />
+                                <h5>Node.js</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <img src={meter1} alt="Web Development" />
+                                <h5>Next.js</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="Brand Identity" />
+                                <h5>Firebase</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Logo Design" />
+                                <h5>Redux</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter1} alt="Web Development" />
+                                <h5>Tailwind CSS</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="Brand Identity" />
+                                <h5>Git & GitHub</h5>
                             </div>
                         </Carousel>
                     </div>
                 </div>
             </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+        <img className="background-image-left" src={colorSharp} alt="Background" />
     </section>
-  )
+  );
 }
