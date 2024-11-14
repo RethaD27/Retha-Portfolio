2500b1b28b0c642314f3a9d33e4dd1290deff6aa
Rethabile <rethadiale27@gmail.com>
2024-11-14 06:13:38 +0200

    Update Banner component: modify paragraph text and remove unnecessary lines.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -58,9 +58,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Rethabile Diale`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Software Engineer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>I am passionate about Full-Stack web
-        development, Data Science, Mathematics, and Statistics. Furthermore, I am also a professional
-        long-distance runner and a Math tutor.</p>
+                  <p>A passionate full-stack junior software engineer seeking a role to apply my skills in React, JavaScript, and back-end technologies to develop high-quality, scalable software solutions. Eager to contribute to engineering efforts by building seamless, user-focused applications across the entire stack.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
